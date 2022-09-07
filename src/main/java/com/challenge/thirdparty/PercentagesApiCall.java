74d201fa5aa3a8efffc0b4cf7de94a57b1200370
Guillermo Túnez <guillermo.tunez@ank.app>
2022-09-07 12:39:54 -0300

    Code review comments fix.

--- src/main/java/com/challenge/thirdparty/PercentagesApiCall.java
+++ src/main/java/com/challenge/thirdparty/PercentagesApiCall.java
@@ -5,8 +5,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Headers;
 
 public interface PercentagesApiCall {
-	
-	@GET(PercentagesEndpoints.ENDPOINT_SUM_PERCENTAGE)
+
+    @GET(PercentagesEndpoints.ENDPOINT_SUM_PERCENTAGE)
     @Headers({"Content-Type: application/json", "User-Agent: Challenge"})
     Call<PercentageDto> getSumPercentage();
 
--- src/main/java/com/challenge/thirdparty/PercentagesApiCallImpl.java
+++ src/main/java/com/challenge/thirdparty/PercentagesApiCallImpl.java
@@ -11,33 +11,33 @@ import static com.challenge.thirdparty.PercentagesEndpoints.ENDPOINT_SUM_PERCENT
 
 @Service
 public class PercentagesApiCallImpl {
-	
-	@Autowired
-	private PercentagesApiCall percentagesApiCall;
-	
-	/**
+
+    @Autowired
+    private PercentagesApiCall percentagesApiCall;
+
+    /**
      * Just for testing purposes!
      */
-	void setRestService(PercentagesApiCall percentagesApiCall) {
-		this.percentagesApiCall = percentagesApiCall;
-	}
+    void setRestService(PercentagesApiCall percentagesApiCall) {
+        this.percentagesApiCall = percentagesApiCall;
+    }
 
-	public PercentageDto getSumPercentage() {
-		Call<PercentageDto> call = percentagesApiCall.getSumPercentage();
-		Response<PercentageDto> response = null;
-		try {
-			response = call.execute();
-		} catch (IOException e) {
-			// log.error
-		}
-		if (response != null && !response.isSuccessful()){
-			// log.error
-		}
-		if (response.isSuccessful()) {
-			return response.body();
-		} else {
-			throw new RuntimeException("Not successful response from ApiCall: " + ENDPOINT_SUM_PERCENTAGE +
-					"HttpStatusCode: " + response.code() + "ErrorBody: " + response.errorBody());
-		}
-	}
+    public PercentageDto getSumPercentage() {
+        Call<PercentageDto> call = percentagesApiCall.getSumPercentage();
+        Response<PercentageDto> response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            // log.error
+        }
+        if (response != null && !response.isSuccessful()) {
+            // log.error
+        }
+        if (response.isSuccessful()) {
+            return response.body();
+        } else {
+            throw new RuntimeException("Not successful response from ApiCall: " + ENDPOINT_SUM_PERCENTAGE +
+                    "HttpStatusCode: " + response.code() + "ErrorBody: " + response.errorBody());
+        }
+    }
 }
